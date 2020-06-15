7f7fc5a739fcaca0b08e34d624f12aed5e280537
CharlesF <61162501+CharlesFricaud@users.noreply.github.com>
2020-06-15 09:26:22 +0200

    Add files via upload

--- scripts/news.js
+++ scripts/news.js
@@ -6,46 +6,162 @@ var recherche_courante;
 var recherche_courante_news = [];
 
 saisie = document.getElementById("zone_saisie");
+recherches_S = document.getElementById("recherches-stockees");
+resultats = document.getElementById("resultats");
+wait = document.getElementById("wait");
+
+/**
+ * Fonction utilitaire qui cherche si l'objet résultat (o) est bien
+ * présent dans dans le tableau des résultats (t)
+ * => Si un résultat a exactement le même titre et la même date,
+ *     la fonction retourne l'index de celui-ci dans le tableau
+ * => Sinon, la fonction retourne -1
+*/
+function indexOfResultat(t, o){
+	try {
+    var limit = t.length;
+  } catch (e) {
+    var limit = 0;
+  }
+	var trouve = false;
+	var i = 0;
+	while( (!trouve) && (i<limit) ){
+		var c = t[i];
+		if ((c.titre == o.titre) && (c.date == o.date)){
+			trouve = true;
+		}
+		i++;
+	}
+	if (trouve) { return (i-1); }
+	else { return -1; }
+}
 
 
 function ajouter_recherche() {
-  if(!recherches.indexOf("saisie")){
-  	recherches.appendChild(saisie);
-    console.log(recherches),
+  if(recherches.indexOf(saisie.value) < 0){
+  	  recherches.push(saisie.value);
+      recherches_S.innerHTML+=('<p class="titre-recherche"><label onclick="selectionner_recherche(this)">'+saisie.value+'</label><img src="img/croix30.jpg" onclick="supprimer_recherche(this)" class="icone-croix"/></p>');
+      localStorage.setItem("recherches",recherches);
+  }
+  else
+  {
+  	  console.log("already saved");
   }
 }
 
 
 function supprimer_recherche(elt) {
-	//TODO ...
+  var parent = elt.parentNode;
+  var id = recherches.indexOf(parent.innerText);
+  recherches.splice(id,1);
+  var suparent = parent.parentNode;
+  suparent.removeChild(parent);
+  localStorage.setItem("recherches",recherches);
+  localStorage.removeItem(parent.innerText);
 }
 
 
 function selectionner_recherche(elt) {
-	//TODO ...
+  recherche_courante_news = [];
+  resultats.innerHTML="";
+  wait.style.display="block";
+  var parent = elt.parentNode;
+  var content = parent.innerText;
+  saisie.value=content;
+
+  recherche_courante = saisie.value;
+  if(recherche_courante in localStorage){
+    recherche_courante_news =  JSON.parse(localStorage.getItem(recherche_courante));
+    for (var i = 0; i < recherche_courante_news.length; i++) {
+      resultats.innerHTML+=('<p class="titre_result"><a class="titre_news" href='+recherche_courante_news[i].url+'target="_blank">'+recherche_courante_news[i].titre+'</a><span class="date_news">'+recherche_courante_news[i].date+'</span><span class="action_news" onclick="supprimer_nouvelle(this)"><img src="img/disk15.jpg"/></span></p>');
+    }
+  }
+  else{
+    console.log("aucune annonce sauvegardée pour cette recherche");
+  }
+  wait.style.display="none";
 }
 
 
 function init() {
-	//TODO ...
+  if(localStorage.getItem("recherches"))
+  {
+    var stockage = localStorage.getItem("recherches");
+    stockage = stockage.split(",");
+    for (var i = 0; i < stockage.length; i++) {
+      recherches.push(stockage[i]);
+      recherches_S.innerHTML+=('<p class="titre-recherche"><label onclick="selectionner_recherche(this)">'+stockage[i]+'</label><img src="img/croix30.jpg" onclick="supprimer_recherche(this)" class="icone-croix"/></p>');
+    }
+  }
 }
 
 
 function rechercher_nouvelles() {
-	//TODO ...
+	resultats.innerHTML="";
+  wait.style.display="block";
+  var xhr = new XMLHttpRequest();
+
+  xhr.onreadystatechange = function() {
+      if(this.readyState == 4 && this.status == 200){
+        var data = this.response;
+        for (var i = 0; i < data.length; i++) {
+          resultats.innerHTML+=('<p class="titre_result"><a class="titre_news" href='+data[i].url+'target="_blank">'+data[i].titre+'</a><span class="date_news">'+data[i].date+'</span><span class="action_news" onclick="sauver_nouvelle(this)"><img src="img/horloge15.jpg"/></span></p>');
+        }
+      }
+  };
+  xhr.open("GET","https://cavi.alwaysdata.net/search-internships.php?data="+recherche_courante,true);
+  xhr.responseType = "json";
+  xhr.send();
+  wait.style.display="none";
 }
 
 
-function maj_resultats(res) {
-	//TODO ...
+class Annonce{
+  constructor(titre, date, url){
+    this.titre = titre;
+    this.date = date;
+    this.url = url;
+  }
 }
 
 
 function sauver_nouvelle(elt) {
-	//TODO ...
+  var image = elt.lastChild;
+  var parent = elt.parentNode;
+  image.setAttribute('src','img/disk15.jpg');
+  elt.setAttribute('onclick',"supprimer_nouvelle(this)");
+
+  var titre = parent.getElementsByTagName("a")[0].innerHTML;
+  var date = parent.getElementsByTagName("span")[0].innerHTML;
+  var url = parent.getElementsByTagName("a")[0].href;
+  var annonce = new Annonce(titre, date, url);
+
+  if(indexOfResultat(recherche_courante_news, annonce) == -1){
+  	  recherche_courante_news.push(annonce);
+      localStorage.setItem(recherche_courante,JSON.stringify(recherche_courante_news));
+  }
+  else
+  {
+  	  console.log("already saved");
+  }
 }
 
 
 function supprimer_nouvelle(elt) {
-	//TODO ...
+  var image = elt.lastChild;
+  var parent = elt.parentNode;
+  image.setAttribute('src','img/horloge15.jpg');
+  elt.setAttribute('onclick',"sauver_nouvelle(this)");
+
+  var titre = parent.getElementsByTagName("a")[0].innerHTML;
+  var date = parent.getElementsByTagName("span")[0].innerHTML;
+  var url = parent.getElementsByTagName("a")[0].href;
+  var annonce = new Annonce(titre, date, url);
+
+  try {
+    recherche_courante_news.splice(indexOfResultat(recherche_courante_news, annonce), 1);
+    localStorage.setItem(recherche_courante,JSON.stringify(recherche_courante_news));
+  } catch (e) {
+      console.log("problème suppr_nouvelle");
+  }
 }
